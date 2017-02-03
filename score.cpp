153ec932e1e3b32303d2cd903edccf722c0cd833
Hanieh Marvi <Hanieh.Marvi1@gmail.com>
2017-02-03 10:59:58 +0330

    Basic Logic of Score

---
+++ score.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "score.h"
+
+using namespace std;
+
+Score::Score() { value = 0; }
+
+unsigned int Score::getScore() { return value; }
+
+void Score::addScore(Object destroyed) { value += destroyed; }
