9bf933c80ce06f533afede6a01c69550f78f6d21
Kimia Talaei <kimkimia7@gmail.com>
2017-02-03 12:31:50 +0330

    basic logic of fuel class

--- fuel.cpp
+++ fuel.cpp
@@ -1,1 +1,14 @@
+#include "fuel.h"
 
+fuel::fuel(int X, int Y, int Width, int Height)
+{
+    x = X;
+    y = Y;
+    width = Width;
+    height = Height;
+}
+
+void fuel::move_down()
+{
+    y += 5;
+}
