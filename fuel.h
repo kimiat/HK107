879040a42f18317bd580ccbcc307aecfb3c88751
Kimia Talaei <kimkimia7@gmail.com>
2017-02-03 12:33:36 +0330

    basic logic of fuel class

--- fuel.h
+++ fuel.h
@@ -1,5 +1,16 @@
 #ifndef FUEL
 #define FUEL
 
+class fuel{
+    private:
+        int x;
+        int y;
+        int height;
+        int width;
+    public:
+        fuel(int X, int Y, int Width, int Height);
+        void move_down();
+};
+
 #endif // FUEL
 
