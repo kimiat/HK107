b64675d863b519d4cc3a727123371fffb5bf44ba
Kimia Talaei <kimkimia7@gmail.com>
2017-02-04 15:40:37 +0330

    adding graphics

--- missile.h
+++ missile.h
@@ -1,16 +1,24 @@
 #ifndef MISSILE
 #define MISSILE
+#include<QTimer>
+#include<QGraphicsRectItem>
+#include<QObject>
 
-class missile{
+class missile:public QObject,  public QGraphicsRectItem{
+    Q_OBJECT
     private:
         int x;
         int y;
         int height;
         int width;
+        QTimer* timer;
     public:
         missile(int x, int y, int width, int height);
         int get_x();
         int get_y();
+        ~missile();
+        int get_height();
+    public slots:
         void move_up();
 };
 
