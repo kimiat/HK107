4584313c20918f80e3078a1181d3fee2062a82c5
Kimia Talaei <kimkimia7@gmail.com>
2017-02-04 15:44:14 +0330

    adding graphics

--- player.cpp
+++ player.cpp
@@ -1,22 +1,36 @@
 #include "missile.h"
+#include <QKeyEvent>
+#include<QGraphicsScene>
 #include "player.h"
+#define speed 5
+#define m_height 10
+#define m_width 5
+#define p_height 600
+#define p_width 800
+#include<iostream>
+using namespace std;
+extern int width;
+extern int num;
 Player::Player(int x, int y, int width, int height)
 {
     this->x = x;
     this->y = y;
     this->height = height;
     this->width = width;
+    setRect(0,0, width, height);
     fuel = 500;
 }
 
 void Player::move_left()
 {
-    x -= 5;
+    if(x - speed > 0)
+        x -= speed;
 }
 
 void Player::move_right()
 {
-    x += 5;
+    if(x + speed < p_width - width)
+        x += speed;
 }
 
 void Player::inc_fuel()
@@ -38,10 +52,39 @@ int Player::get_y()
     return y;
 }
 
-missile* Player::shoot()
+//int Player::get_height()
+//{
+//    return height;
+//}
+
+//int Player::get_width()
+//{
+//    return width;
+//}
+
+void Player::shoot()
 {
-    int m_height;
-    int m_width;
     missile* m = new missile(x + width/2 - m_width/2, y - m_height, m_width, m_height);
-    return m;
+    m->setPos(m->get_x(), m->get_y());
+    scene()->addItem(m);
+    return;
+}
+
+void Player::keyPressEvent(QKeyEvent *direction)
+{
+    if(direction->key() == Qt::Key_Left)
+    {
+        move_left();
+        setPos(get_x(), get_y());
+    }
+    if(direction->key() == Qt::Key_Right)
+    {
+        move_right();
+        setPos(get_x(), get_y());
+    }
+    if(direction->key() == Qt::Key_Space)
+    {
+        if(num <= 1)
+            shoot();
+    }
 }
