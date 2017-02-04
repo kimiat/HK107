becf687e82a6424f6409cf1581792da7d0eeecfc
Kimia Talaei <kimkimia7@gmail.com>
2017-02-04 15:43:22 +0330

    adding graphics

--- missile.cpp
+++ missile.cpp
@@ -1,11 +1,21 @@
 #include "missile.h"
-
+#include <QTimer>
+#include<iostream>
+#include<QGraphicsScene>
+#define speed 5
+using namespace std;
+int num = 0;
 missile::missile(int x, int y, int width, int height)
 {
+    setRect(0, 0, width, height);
     this->x = x;
     this->y = y;
     this->width = width;
     this->height = height;
+    num++;
+    timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(move_up()));
+    timer->start(10);
 }
 
 int missile::get_x()
@@ -18,7 +28,31 @@ int missile::get_y()
     return y;
 }
 
+int missile::get_height()
+{
+    return height;
+}
+
+int get_num()
+{
+    return num;
+}
+
 void missile::move_up()
 {
-    y -= 5;
+    y -= speed;
+    setPos(get_x(), get_y() - 10);
+    if(get_y() < 0 - get_height())
+    {
+        scene()->removeItem(this);
+        delete this;
+    }
+
 }
+
+missile::~missile()
+{
+    delete timer;
+    num--;
+}
+
