7c17292e46ee4e2374629d3d92161a70e541809e
Kimia Talaei <kimkimia7@gmail.com>
2017-02-06 23:30:05 +0330

    adding panel to the bottom

---
+++ bottom_panel.h
@@ -0,0 +1,5 @@
+#ifndef BOTTOM_PANEL
+#define BOTTOM_PANEL
+
+#endif // BOTTOM_PANEL
+
--- setup.h
+++ setup.h
@@ -8,6 +8,7 @@
 #include <QKeyEvent>
 #include <QWidget>
 #include <QObject>
+#include "bottom_panel.h"
 
 class Setup: public QObject
 {
@@ -16,6 +17,7 @@ private:
     QGraphicsScene* scene;
     Player* player;
     QGraphicsView* view;
+    panel* Panel;
 public:
     Setup();
     QGraphicsScene *getScene();
