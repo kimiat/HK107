dff7caca83fec45a3593c53c46de16d1deb86f78
Hanieh Marvi <Hanieh.Marvi1@gmail.com>
2017-02-07 00:06:16 +0330

    Making random fuel tanks appear on screen

--- fuel.h
+++ fuel.h
@@ -1,16 +1,17 @@
 #ifndef FUEL
 #define FUEL
 
-class fuel{
-    private:
-        int x;
-        int y;
-        int height;
-        int width;
-    public:
-        fuel(int X, int Y, int Width, int Height);
-        void move_down();
+class fuel
+{
+private:
+    int x;
+    int y;
+    int height;
+    int width;
+public:
+    fuel(int X, int Y, int Width, int Height);
+    void move_down();
 };
 
-#endif // FUEL
+#endif
 
--- setup.cpp
+++ setup.cpp
@@ -1,6 +1,7 @@
 #include "setup.h"
 #include "player.h"
 #include "enemy.h"
+#include "tank.h"
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QGraphicsItem>
@@ -13,11 +14,12 @@
 #define p_height 50
 #define p_width 50
 #define down_pannel_h 80
+
 int height = 600;
 int width = 800;
 
 pair<int, int> startingPos = make_pair(100, 500);
-pair<int, int> startingTime = make_pair(2000, 6000);
+pair<int, int> startingTime = make_pair(700, 1500);
 pair<int, int> movingPos = make_pair(50, 500);
 
 using namespace std;
@@ -49,10 +51,6 @@ void Setup::start()
     view->show();
 }
 
-//int xPos;
-//Movement moveType;
-//Object type;
-
 void Setup::makeEnemies()
 {
     int randTime;
@@ -63,38 +61,37 @@ void Setup::makeEnemies()
     timer->start(randTime);
 }
 
-QVector<Enemy*> enemies;
-int counter = 0;
-
 void Setup::newEnemies()
 {
     srand(time(NULL));
-    int randPos, randMove, randType, randStart, randWay;
+    int randPos, randMove, randType, randStart, randWay, randTank;
     Movement moveType;
     randPos = rand() % (startingPos.second - startingPos.first) + startingPos.first;
     randStart = rand() % (movingPos.second - movingPos.first) + movingPos.first;
     randType = rand() % 2;
     randWay = rand() % 2;
+    randTank = rand() % 2;
     randMove = rand() % 10;
-
+    if(randTank)
+    {
+        Tank *tank = new Tank(randPos, 0, 20, 60);
+        this->scene->addItem(tank);
+        return;
+    }
     if(randWay && randMove > 6)
         moveType = LEFT;
     else if(!randWay && randMove > 6)
         moveType = RIGHT;
     else
         moveType = DOWN;
-    counter++;
-    Ship *ship;
-    Helicopter *helicopter;
+    Enemy *enemy;
     if(randType)
     {
-        enemies.push_back(ship);
-        enemies[counter - 1] = new Ship(randPos, 0, 40, 80, randStart, DOWN, moveType);
+        enemy = new Ship(randPos, 0, 40, 80, randStart, DOWN, moveType);
     }
     else
     {
-        enemies.push_back(helicopter);
-        enemies[counter - 1] = new Helicopter(randPos, 0, 60, 60, randStart, DOWN, moveType);
+        enemy = new Helicopter(randPos, 0, 60, 60, randStart, DOWN, moveType);
     }
-    this->scene->addItem(enemies[counter - 1]);
+    this->scene->addItem(enemy);
 }
