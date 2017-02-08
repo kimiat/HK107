d957097ba696e1400169d9b431824a36226853c7
Kimia Talaei <kimkimia7@gmail.com>
2017-02-08 13:56:22 +0330

    completing the logic of fuel

--- fuel.cpp
+++ fuel.cpp
@@ -1,15 +1,31 @@
-//
 #include "fuel.h"
 
-fuel::fuel(int X, int Y, int Width, int Height)
+fuel::fuel()
 {
-    x = X;
-    y = Y;
-    width = Width;
-    height = Height;
+    ifuel_timer = new QTimer();
+    dfuel_timer = new QTimer();
+    fuel_amount = 500;
+    connect(ifuel_timer, SIGNAL(timeout()), this, SLOT(inc_fuel()));
+    connect(dfuel_timer, SIGNAL(timeout()), this, SLOT(dec_fuel()));
+    dfuel_timer->start(300);
 }
 
-void fuel::move_down()
+void fuel::inc_fuel()
 {
-    y += 5;
+    if(fuel_amount < 480)
+    {
+        fuel_amount += 20;
+        emit fuelChanged(fuel_amount);
+    }
+}
+
+void fuel::dec_fuel()
+{
+    if(fuel_amount > 5)
+    {
+        fuel_amount -= 5;
+        emit fuelChanged(fuel_amount);
+    }
+//    x = fuel_amount / 10;
+//    setPos(get_x(), get_y());
 }
--- fuel.h
+++ fuel.h
@@ -1,7 +1,25 @@
-//
-#ifndef FUEL_H
-#define FUEL_H
+#ifndef FUEL
+#define FUEL
 
+<<<<<<< HEAD
+#include<QTimer>
+#include<QObject>
+#include<QGraphicsRectItem>
+class fuel:public QObject, public QGraphicsRectItem{
+    Q_OBJECT
+    private:
+        int fuel_amount;
+        QTimer* ifuel_timer;
+        QTimer* dfuel_timer;
+    signals:
+        void fuelChanged(int changed_amount);
+    public:
+        fuel();
+    public slots:
+        void inc_fuel();
+        void dec_fuel();
+//        void move_down();
+=======
 class fuel
 {
 private:
@@ -12,6 +30,7 @@ private:
 public:
     fuel(int X, int Y, int Width, int Height);
     void move_down();
+>>>>>>> 96ec5d452d3ca53c20011281e22326d84337b887
 };
 
 #endif
